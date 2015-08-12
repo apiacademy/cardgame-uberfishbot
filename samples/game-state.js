911b0024293404f17d85cc6a793a1163ed929e64
Mike Amundsen <mamund@yahoo.com>
2015-08-12 09:57:48 -0400

    initial sample msg

---
+++ samples/game-state.js
@@ -0,0 +1,76 @@
+{
+  "uber" :
+  {
+    "version" : "1.0",
+    "data" : [
+      {
+        "rel" : "self",
+        "url" : "http://example.com/cardgame-state" 
+      },
+      {
+        "id" : "GameID",
+        "value" : "q1w2e3"
+      },
+      {
+        "id" : "Deck,
+        "value" : "w2e3r4"
+      },
+      {
+        "id" : "opponentsLastMove",
+        "value" : "???"
+      },
+      {
+        "id" : "cards_lost",
+        "value" : "???"
+      },
+      {
+        "id" : "cards_requested",
+        "value" : "???"
+      },
+      {
+        "id" : "cards_left",
+        "value" : "???"
+      },
+      {
+        "id" : "Hands",
+        "value" : "???"
+      },
+      {
+        "id" : "number_of_cards_opponent_has",
+        "value" : "???"
+      },
+      {
+        "id" : "my_cards",
+        "value" : "???"
+      },
+      {
+        "id" : "activePlayer",
+        "value" : "???"
+      },
+      {
+        "id" : "gameState",
+        "value" : "???"
+      },
+      {
+        "id" : "timeSinceLastMove",
+        "value" : "???"
+      },
+      {
+        "id" : "Heartbeat",
+        "value" : "???"
+      },
+      {
+        "id" : "Players",
+        "value" : "???"
+      },
+      {
+        "id" : "Books",
+        "value" : "???"
+      },
+      {
+        "id" : "winner",
+        "value" : "???"
+      }      
+    ]
+  }
+}
